03dc8650e316eccc6213a4c8683d48818e645294
memothelemo <memothelemo@protonmail.com>
2022-10-26 08:46:32 +0800

    Refactor to sync time!

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,12 +1,1 @@
 pub mod instance;
-
-use id_arena::{Arena, Id};
-use instance::InstanceType;
-use std::sync::Arc;
-use tokio::sync::{Mutex, RwLock};
-
-/// Multi-threaded safe reference of [Instance](Instance).
-pub type InstanceRef = Id<Arc<RwLock<dyn InstanceType + 'static>>>;
-
-/// A collection of all stored instances.
-pub type InstanceArena = Arc<RwLock<Arena<Arc<RwLock<dyn InstanceType + 'static>>>>>;
