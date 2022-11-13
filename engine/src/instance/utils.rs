a438d2fbabba48b4037c23bb3596241167e1ca74
memothelemo <memothelemo@protonmail.com>
2022-11-14 03:09:27 +0800

    Fix rustc lint warnings

--- engine/src/instance/utils.rs
+++ engine/src/instance/utils.rs
@@ -4,11 +4,11 @@ use std::any::Any;
 pub unsafe fn unsafe_transmute<T: Any>(obj: &dyn AnyInstance) -> &T {
     let unsafe_ptr = obj as &dyn std::any::Any as *const dyn std::any::Any;
     let unsafe_ptr = unsafe_ptr as *const T;
-    unsafe { &*unsafe_ptr }
+    &*unsafe_ptr
 }
 
 pub unsafe fn unsafe_transmute_mut<T: Any>(obj: &mut dyn AnyInstance) -> &mut T {
     let unsafe_ptr = obj as &mut dyn std::any::Any as *mut dyn std::any::Any;
     let unsafe_ptr = unsafe_ptr as *mut T;
-    unsafe { &mut *unsafe_ptr }
+    &mut *unsafe_ptr
 }
